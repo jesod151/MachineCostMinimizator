c7f0eec7fc37f052a11b934cff0627e3c31312b1
jesod151 <jesod151@gmail.com>
2023-01-29 12:46:51 -0600

    JUnit tests class added

---
+++ src/test/java/com/cost/minimizator/demo/services/tests/MinimizatorServiceImplTests.java
@@ -0,0 +1,14 @@
+package com.cost.minimizator.demo.services.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MinimizatorServiceImplTests {
+
+	@Test
+	void test() {
+		System.out.println("tests");
+	}
+
+}
